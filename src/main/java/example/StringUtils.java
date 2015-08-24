0334dd4cb691abea0b5de182d5caa4f527ed2764
Yosuke Otsuka <yosuke.otsuka@develhack.com>
2015-08-25 07:54:40 +0900

    Add theme

--- src/main/java/example/StringUtils.java
+++ src/main/java/example/StringUtils.java
@@ -1,9 +1,22 @@
 package example;
 
+/**
+ * Example of asciidoclet.
+ *
+ * @see https://github.com/asciidoctor/asciidoclet[asciidoctor/asciidoclet]
+ */
 public class StringUtils {
-    // tag::contains[]
-    public boolean contains(String haystack, String needle) {
-        return haystack.contains(needle);
-    }
-    // end::contains[]
+
+	/**
+	 * Returns whether the `haystack` containing the `needle`.
+	 *
+	 * @param haystack a haystack
+	 * @param needle a needle
+	 * @return `true` if the `haystack` contains the `needle`
+	 */
+	// tag::contains[]
+	public boolean contains(String haystack, String needle) {
+		return haystack.contains(needle);
+	}
+	// end::contains[]
 }
